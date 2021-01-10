ba0d1ee3b47a8c52e8847a39c62d90b4bd56dd20
Katurene <katurene7@gmail.com>
2021-01-11 00:12:03 +0300

    add home page

---
+++ ShopAvto/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopAvto.Data.Interfaces;
+using ShopAvto.ViewModels;
+
+namespace ShopAvto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAllCars CarRepository;
+      
+        public HomeController(IAllCars carRepository)
+        {
+            CarRepository = carRepository;          
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                favCars = CarRepository.getFavCars
+            };
+            return View(homeCars);//все товары кот долж отобр на гл стр
+        }
+    }
+}
---
+++ ShopAvto/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopAvto.Data.Models;
+
+namespace ShopAvto.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> favCars { get; set; }
+    }
+}
